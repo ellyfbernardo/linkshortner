6ef371b2e6edf960af5ce3b0f7b41ef8357fd3c4
Ellyf Bernardo <ellyfbernardo12@gmail.com>
2022-11-10 00:56:06 -0300

    fuck off

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,17 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import {BrowserRouter, Routes, Route, HashRouter} from 'react-router-dom'
 import Error from './pages/Error'
 import Home from './pages/Home'
 import Links from './pages/Links'
 
 function RoutesApp(){
     return(
-        <BrowserRouter>
+        <HashRouter>
             <Routes>
                 <Route path='/' element={<Home/>}/>
                 <Route path='/links' element={<Links/>}/>
                 <Route path='*' element={<Error/>}/>
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     )
 }
 export default RoutesApp;
